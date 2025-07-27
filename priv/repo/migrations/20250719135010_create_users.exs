b5d965e5df9baea7a2607306eb735dc23c75fb09
boterop <boterop22@gmail.com>
2025-07-26 21:31:26 -0500

    Create unique index for email

--- priv/repo/migrations/20250719135010_create_users.exs
+++ priv/repo/migrations/20250719135010_create_users.exs
@@ -5,10 +5,12 @@ defmodule EricApi.Repo.Migrations.CreateUsers do
     create table(:users, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :name, :string
-      add :email, :string, unique: true
+      add :email, :string
       add :password, :string
 
       timestamps(type: :utc_datetime)
     end
+
+    create unique_index(:users, [:email])
   end
 end
